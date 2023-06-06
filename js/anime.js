4200f29da366b2e001910c5694aaba6df51e642d
Ednesor <edgar.funes2001@gmail.com>
2023-06-06 02:59:25 -0300

    feat: add desktop

---
+++ js/anime.js
@@ -0,0 +1,10 @@
+// const productCard = document.getElementById('navbar');
+// const productButton = document.getElementById('navbar');
+
+// anime({
+//     targets: '.gradient',
+//     easing: 'easeInOutQuad',
+//     duration: 60000,
+//     loop: true,
+//     direction: 'alternate',
+//   });
